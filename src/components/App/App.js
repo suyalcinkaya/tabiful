a243f0e2750e7696a3eb7611020745619a581375
Onur Şuyalçınkaya <onur.suyalcinkaya@Onur-Suyalcnkaya-MBP.local>
2018-09-10 16:15:17 +0300

    update, bugfix
    - node-sass-chokidar and npm-run-all dependencies removed.
    - Shortcut is Ctrl+M now.
    - styled-components dependency added.
    - Bugs fixed.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,123 @@
 
 import React, { Component } from 'react';
 import Fuse from 'fuse.js';
-import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  display: flex;
+  width: 400px;
+  max-height: 600px;
+  color: rgba(0, 0, 0, 0.8);
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  flex-direction: column;
+`;
+
+const Input = styled.input.attrs({
+  type: "text", 
+  placeholder: "Jump to..", 
+  spellcheck: "false", 
+  autoFocus: "true",
+})`
+  width: 100%;
+  font-size: 20px;
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.8);
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 0px;
+  z-index: 0;
+  padding: 16px 24px;
+  border-width: initial;
+  border-style: none;
+  border-color: initial;
+  border-image: initial;
+  outline: 0px;
+  flex: 0 0 auto;
+`;
+
+const TabList = styled.div`
+  overflow-y: auto;
+  padding: 12px;
+  flex: 1 1 auto; 
+`;
+
+const TabContainer = styled.div`
+  display: flex;
+  height: 40px;
+  cursor: pointer;
+  -webkit-box-align: center;
+  align-items: center;
+  user-select: none;
+  padding: 0px 6px;
+  border-radius: 3px;
+  -webkit-transition: 0.1s;
+  transition: 0.1s;
+
+  &:hover {
+    background-color: #4ada99;
+    color: #fff;
+
+    svg {
+      display: block;
+    }
+  }
+`;
+
+const FavIcon = styled.span`
+  box-sizing: content-box;
+  width: 16px;
+  height: 16px;
+  background-color: white;
+  padding: 3px;
+  margin: 3px;
+  border-radius: 3px;
+  flex: 0 0 auto; 
+`;
+
+const favIconPlaceholder = (
+  <svg viewBox="0 0 16 16" fill="none" stroke="#5A5A5A" strokeWidth="1">
+    <polygon points="3.5,1.5 8.5,1.5 12.5,5.5 12.5,14.5 3.5,14.5" />
+    <polyline points="8.5,1.5 8.5,5.5 12.5,5.5" />
+  </svg>
+);
+
+const Title = styled.span`
+  font-size: 14px;
+  line-height: 40px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  margin: 0px 6px;
+  overflow: hidden;
+  flex: 1 1 auto;
+`;
+
+const CloseIcon = styled.svg.attrs({
+  viewBox :"0 0 16 16",
+})`
+  display: none;
+  box-sizing: content-box;
+  width: 16px;
+  height: 16px;
+  stroke: currentcolor;
+  stroke-width: 1.5;
+  stroke-linecap: round;
+  margin: 3px;
+  padding: 3px;
+  border-radius: 3px;
+  flex: 0 0 auto;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+    -webkit-transition: 0.1s;
+    transition: 0.1s;
+  }
+`;
+
+const NoResult = styled.span`
+  display: block;
+  font-size: 14px;
+  text-align: center;
+  padding: 12px;
+`;
 
 class App extends Component {
   state = {
@@ -16,7 +132,8 @@ class App extends Component {
     setTimeout(() => {
       const promises = [
         new Promise(resolve => {
-          chrome.tabs.query({}, tabs => resolve(tabs));
+          // Remove currentWindow: true to get from all windows
+          chrome.tabs.query({ currentWindow: true }, tabs => resolve(tabs));
         }),
         new Promise(resolve => {
           chrome.windows.getCurrent({}, ({ id }) => resolve(id));
@@ -25,14 +142,13 @@ class App extends Component {
 
       Promise.all(promises).then(([tabs, currentWindowId]) => {
         const highlightedIndex = this.getActiveIndex(tabs, currentWindowId);
-
-        this.setState({
-          tabs: tabs,
-          currentWindowId: currentWindowId,
-          highlightedIndex: highlightedIndex,
+        this.setState({ 
+          tabs,
+          currentWindowId,
+          highlightedIndex,
         });
       });
-    }, 200);
+    }, 250);
   }
 
   getActiveIndex = (tabs, currentWindowId) => {
@@ -44,19 +160,9 @@ class App extends Component {
       return tabs;
     }
 
-    const options = {
-      threshold: 0.5,
-      keys: ['title', 'url'],
-    };
-
-    const fuse = new Fuse(tabs, options);
-
+    const fuse = new Fuse(tabs, { threshold: 0.5, keys: ['title', 'url']} );
     return fuse.search(filterValue);
   };
-
-  getActiveIndex = (tabs, currentWindowId) => {
-    tabs.findIndex(tab => tab.windowId === currentWindowId && tab.active);
-  }
 
   handleFilterChange = ({ target: { value } }) => {
     const { tabs, currentWindowId } = this.state;
@@ -68,11 +174,8 @@ class App extends Component {
   };
 
   handleTabSelect = tab => {
-    chrome.windows.update(tab.windowId, { focused: true }, () => {
-      chrome.tabs.update(tab.id, { active: true });
-      chrome.tabs.update(tab.id, { highlighted: true });
-      chrome.tabs.update(tab.id, { selected: true });
-    });
+    chrome.windows.update(tab.windowId, { focused: true });
+    chrome.tabs.update(tab.id, { active: true });
   };
 
   handleTabRemove = tab => {
@@ -86,30 +189,48 @@ class App extends Component {
     });
   };
 
+  handleKeyPress = (event) => {
+    console.log(event);
+  }
+
   render() {
     const tabs = this.filterTabs(this.state.tabs, this.state.filterValue);
-    console.log(tabs);
 
     return (
-      <div className="app-container">
-        <input type="text" placeholder="Jump to.." autoFocus value={this.state.filterValue} onChange={this.handleFilterChange} />
-        <div className="tabs-wrapper">
+      <AppContainer>
+        <Input value={this.state.filterValue} onChange={this.handleFilterChange} />
+        {
+          tabs && (
+          <TabList>
           {
-            tabs && tabs.map(tab => 
-              <div className="tabs-item" onClick={() => {this.handleTabSelect(tab)}}>
-                <span className="favIcon-wrapper">
-                  <img src={tab.favIconUrl} alt width="100%" height="100%" />
-                </span>
-                <span className="tab-title">{tab.title}</span>
-                <svg viewBox="0 0 16 16" onClick={() => {this.handleTabRemove(tab)}}>
+            tabs.length > 0 
+            ? tabs.map(tab => 
+              ( 
+              <TabContainer onClick={() => {this.handleTabSelect(tab)}} onKeyPress={() => {this.handleKeyPress}}>
+                <FavIcon>
+                  {
+                    /^https?:\/\//.test(tab.favIconUrl) 
+                    ? ( <img src={tab.favIconUrl} alt="" width="100%" height="100%" /> ) 
+                    : ( favIconPlaceholder )
+                  }
+                </FavIcon>
+                <Title>{tab.title}</Title>
+                <CloseIcon onClick={(event) => {
+                  this.handleTabRemove(tab);
+                  event.stopPropagation();
+                }}>
                   <line x1="3" y1="3" x2="13" y2="13"></line>
                   <line x1="13" y1="3" x2="3" y2="13"></line>
-                </svg>
-              </div>
+                </CloseIcon>
+              </TabContainer>
+              )
             )
+            : ( <NoResult>No matches found.</NoResult> )
           }
-        </div>
-      </div>
+          </TabList>
+          )
+        }
+      </AppContainer>
     );
   }
 }
